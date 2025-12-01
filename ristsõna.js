7238be4ca845e4826216f54085b1c4901a8adb53
Robin Mäeots <robinmaeots@ut.ee>
2025-12-01 18:40:00 +0200

    muutsin

---
+++ ristsõna.js
@@ -0,0 +1,58 @@
+const answers = {
+    1: 'N',
+    2: 'O',
+    3: 'K',
+    4: 'K',
+    5: 'T',
+    6: 'A',
+    7: 'I',
+    8: 'M',
+    9: 'E',
+    10: 'N',
+    11: 'E',
+    12: 'L',
+    13: 'I',
+    14: 'K',
+    15: 'I',
+    16: 'L',
+    17: 'P',
+    18: 'I',
+    19: 'K',
+    20: 'U',
+    21: 'U',
+    22: 'L',
+    23: 'E',
+    24: 'L',
+    25: 'O',
+    26: 'O',
+    27: 'D',
+    28: 'U',
+    29: 'S',
+    30: 'E',
+    31: 'S',
+    32: 'U',
+    33: 'N',
+    34: 'D',
+    35: 'M',
+    36: 'U',
+    37: 'N',
+    38: 'E',
+    39: 'B'
+};
+
+const inputs = document.querySelectorAll('input.inpt');
+
+inputs.forEach(input => {
+    input.addEventListener('input', () => {
+        const id = parseInt(input.parentElement.id);
+        const correct = answers[id];
+
+        if(correct) {
+            if(input.value.toUpperCase() === correct.toUpperCase()) {
+                input.style.color = 'green';
+            } else {
+                input.style.color = 'red';
+            }
+        }
+    });
+});
